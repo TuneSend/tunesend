0e2e56d005bdad603f8a6ee9647b5355f4f970c4
Jovan Williams <jovanwilliams1995@gmail.com>
2017-12-21 21:36:09 -0500

    replaced original files and pusing the front end of the app

--- App.js
+++ App.js
@@ -1,38 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React, {Component} from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Heading from './heading.js';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+    <View style={styles.container}>
+      <Heading />
+    </View>
     );
   }
 }
@@ -40,18 +15,8 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#FFFFFF',
   },
 });
+
+AppRegistry.registerComponent('App', ()=> App );
---
+++ heading.js
@@ -0,0 +1,126 @@
+import React, {Component} from 'react';
+import {	Animated,
+			Image,
+			ScrollView,
+			AppRegistry,
+	 		StyleSheet,
+	 		Text,
+	 		View,
+	 	} from 'react-native';
+
+const HEADER_MAX_HEIGHT = 200;
+const HEADER_MIN_HEIGHT = 60;
+const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+
+export default class Heading extends React.Component {
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			scrollY: new Animated.Value(0),
+		};
+	}
+
+	_renderScrollViewContent() {
+		const data = Array.from({length:16});
+		return(
+			<View style={styles.ScrollViewContent}>
+				{data.map((_, i)=>
+					<View key={i} style={styles.row}>
+						<Text>{i}</Text>
+					</View>
+				)}
+			</View>
+			);
+	}
+
+	render() {
+		const headerHeight = this.state.scrollY.interpolate({
+			inputRange: [0, HEADER_SCROLL_DISTANCE],
+			outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+			extrapolate: 'clamp',
+		});
+
+		const imageOpacity = this.state.scrollY.interpolate ({
+			inputRange: [0,HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
+			outputRange: [1, 1, 0],
+			extrapolate: 'clamp',
+		});
+
+		const imageTranslate = this.state.scrollY.interpolate({
+			inputRange: [0, HEADER_SCROLL_DISTANCE],
+			outputRange: [0, -50],
+			extrapolate: 'clamp',
+		});
+
+		return (
+		<View style={styles.fill}>
+			<ScrollView style={styles.fill} 
+						scrollEventThrottle={16}
+						onScroll={Animated.event(
+								[{nativeEvent: {contentOffset: {y:this.state.scrollY}}}]
+							)}
+						>
+				{this._renderScrollViewContent()}
+			</ScrollView>
+			<Animated.View style={[styles.header, {height: headerHeight}]}>
+				<Image source ={require('./image/icon.png')} />
+				<Animated.Image
+					style={[
+						styles.backgroundImage,
+						{opacity: imageOpacity, transform: [{translateY: imageTranslate}]},
+						]}
+					source = {require('./image/TuneSend.png')}
+					/>
+			</Animated.View>
+		</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	fill: {
+		flex: 1,
+	},
+	row: {
+		height: 40,
+		margin: 16,
+		backgroundColor: '#D3D3D3',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	header: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		backgroundColor: '#03A9F4',
+		overflow: 'hidden',
+	},
+	bar: {
+		marginTop: 28,
+		height: 32,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		backgroundColor: 'transparent',
+		color: 'white',
+		fontSize: 18,
+	},
+	ScrollViewContent: {
+		marginTop: HEADER_MAX_HEIGHT,
+	},
+	backgroundImage: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		right: 0,
+		width: null,
+		height: HEADER_MAX_HEIGHT,
+		resizeMode: 'cover',
+	},
+});
+
+AppRegistry.registerComponent('Heading', ()=> Heading);
