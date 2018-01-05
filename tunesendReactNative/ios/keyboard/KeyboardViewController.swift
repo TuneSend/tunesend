da8573fd5cdb979c02e5f72e241b0f1304438d88
Michael <mhaviv18@gmail.com>
2018-01-04 23:00:57 -0500

    added ibaction for shift key, attempted to fix more stuff in view

--- tunesendReactNative/ios/keyboard/KeyboardViewController.swift
+++ tunesendReactNative/ios/keyboard/KeyboardViewController.swift
@@ -17,10 +17,10 @@ extension UIButton {
     
     let pulse = CASpringAnimation(keyPath: "transform.scale")
     pulse.duration = 0.6
-    pulse.fromValue = 0.95
+    pulse.fromValue = 0.90
     pulse.toValue = 1.0
     pulse.autoreverses = true
-    pulse.repeatCount = 2
+    pulse.repeatCount = 1
     pulse.initialVelocity = 0.5
     pulse.damping = 1.0
     
@@ -94,13 +94,6 @@ class KeyboardViewController: UIInputViewController {
   
   
   
-  
-  @IBAction func hideKeyboard() {
-    
-    dismissKeyboard()
-    
-  }
-  
   @IBAction func deleteText() {
     
     let proxy = textDocumentProxy
@@ -130,6 +123,12 @@ class KeyboardViewController: UIInputViewController {
     
     //start of keypress action
     sender.pulsate()
+    
+  }
+  
+  @IBAction func shiftButton() {
+    
+    
     
   }
   
